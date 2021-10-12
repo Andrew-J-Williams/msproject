8cd2b775f0aeb66a80ba4a77ccabd7d787797bab
Andrew Williams <1williamsaj@gmail.com>
2021-10-12 13:44:34 -0500

    Added Slf4j and commented out a log.info since an error occurred.

--- src/main/java/com/msproject/department/controller/DepartmentController.java
+++ src/main/java/com/msproject/department/controller/DepartmentController.java
@@ -1,20 +1,27 @@
 package com.msproject.department.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.msproject.department.entity.Department;
 import com.msproject.department.service.DepartmentService;
 
+import lombok.extern.slf4j.Slf4j;
+
 @RestController
 @RequestMapping("/department") // We include this as our base path when making REST calls.
+@Slf4j // This annotation adds a logger to our class.
 public class DepartmentController {
 	
 	@Autowired
 	private DepartmentService departmentService;
 	
-	public Department saveDepartment(Department department) {
+	@PostMapping("/") // This request will help save our department to the repository.
+	public Department saveDepartment(@RequestBody Department department) {
+		//log.info("Inside saveDepartment method of DepartmentController");
 		return departmentService.saveDepartment(department);
 	}
 	
