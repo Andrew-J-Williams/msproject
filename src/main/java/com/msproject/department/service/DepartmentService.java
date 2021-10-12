9d99825d304e1b43e707fab05043f8863ab834db
Andrew Williams <1williamsaj@gmail.com>
2021-10-12 13:25:27 -0500

    Added repository to service class, autowired repository

--- src/main/java/com/msproject/department/service/DepartmentService.java
+++ src/main/java/com/msproject/department/service/DepartmentService.java
@@ -1,8 +1,14 @@
 package com.msproject.department.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-@Service
+import com.msproject.department.repository.DepartmentRepository;
+
+@Service // We include this annotation with every service class
 public class DepartmentService {
+	
+	@Autowired
+	private DepartmentRepository departmentRepository;
 	
 }
