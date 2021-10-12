a4a2596136e2e460b603afad34b51250177c4aeb
Andrew Williams <1williamsaj@gmail.com>
2021-10-12 13:33:45 -0500

    Added 'saveDepartment' method to both controller and service

--- src/main/java/com/msproject/department/controller/DepartmentController.java
+++ src/main/java/com/msproject/department/controller/DepartmentController.java
@@ -1,14 +1,21 @@
 package com.msproject.department.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.msproject.department.entity.Department;
 import com.msproject.department.service.DepartmentService;
 
 @RestController
+@RequestMapping("/department") // We include this as our base path when making REST calls.
 public class DepartmentController {
 	
 	@Autowired
 	private DepartmentService departmentService;
+	
+	public Department saveDepartment(Department department) {
+		return departmentService.saveDepartment(department);
+	}
 	
 }
--- src/main/java/com/msproject/department/service/DepartmentService.java
+++ src/main/java/com/msproject/department/service/DepartmentService.java
@@ -3,6 +3,7 @@ package com.msproject.department.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.msproject.department.entity.Department;
 import com.msproject.department.repository.DepartmentRepository;
 
 @Service // We include this annotation with every service class
@@ -10,5 +11,11 @@ public class DepartmentService {
 	
 	@Autowired
 	private DepartmentRepository departmentRepository;
+
+	public Department saveDepartment(Department department) {
+		// We copied this method from the controller to the service class, modifying the return object as the repository 
+		
+		return departmentRepository.save(department);
+	}
 	
 }
