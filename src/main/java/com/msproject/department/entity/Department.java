0c50e3bc3e8161e0a973f5250c6b4a02bd9e1605
Andrew Williams <1williamsaj@gmail.com>
2021-10-12 13:02:48 -0500

    Added annotations to entity file.

--- src/main/java/com/msproject/department/entity/Department.java
+++ src/main/java/com/msproject/department/entity/Department.java
@@ -5,7 +5,14 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 @Entity // Whenever we create a class within an entity, we always use '@Entity' to indicate this class is an entity.
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Department {
 	
 	@Id // Sets 'departmentId' as the primary key...
