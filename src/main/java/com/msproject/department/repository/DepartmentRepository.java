de16a770abf48523d48c153f78e3d4071327be67
Andrew Williams <1williamsaj@gmail.com>
2021-10-12 13:16:10 -0500

    Added comment

--- src/main/java/com/msproject/department/repository/DepartmentRepository.java
+++ src/main/java/com/msproject/department/repository/DepartmentRepository.java
@@ -2,7 +2,7 @@ package com.msproject.department.repository;
 
 import com.msproject.department.entity.*;
 import org.springframework.stereotype.Repository;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaRepository; // We import JpaRepository
 
 @Repository
 public interface DepartmentRepository extends JpaRepository<Department, Long> {
